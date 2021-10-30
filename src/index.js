1f0588b9f9b2ed3d38e56b25fa5664d12be7192e
Erik <ErikCrDv@outlook.com>
2021-10-29 21:13:11 -0500

    Inicial

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import { saludar } from './js/componentes';
 import './style.css';
 
-const nombre = 'Erik';
-saludar(nombre);
+import { Todo, TodoList } from './classes';
+import { crearToDoHtml } from './js/componentes';
+
+export const todoList = new TodoList();
+todoList.todos.forEach(crearToDoHtml);
+console.log(todoList.todos);
